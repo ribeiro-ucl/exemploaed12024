e51633f9508d2909129bfaa1f61f67454a907351
André Ribeiro <53034-andrers@users.noreply.replit.com>
2024-05-28 12:02:34 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,33 @@
+//Bibliotecas (namespaces) necessários para a manipulação de arquivos.
+using System;
+using System.IO;
+using System.Text;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int soma = 0;
+    int qtdNumeros=0;
+    float media = 0f;
+    
+    //Manipulador:
+    FileStream meuArq = new FileStream("meus_arqs/nivel2/dados.txt", FileMode.Open, FileAccess.Read);
+
+    //Leitor do arquivo:
+    StreamReader sr = new StreamReader(meuArq, Encoding.UTF8);
+
+    //Enquanto não chegar ao final do arquivo:
+    while(!sr.EndOfStream){
+      string str = sr.ReadLine();   
+      soma = soma + int.Parse(str);
+      qtdNumeros++;
+
+      Console.WriteLine(str);
+    }
+    sr.Close();
+    meuArq.Close();
+
+    media = (float)soma/qtdNumeros;
+    Console.WriteLine($"Media: {media}");
+
+  }
+}
